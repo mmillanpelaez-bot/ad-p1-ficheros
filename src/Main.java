d3d580d924c43b60d844e2b6554b07b5c3fb3255
Felipe <mmillanpelaez@danielcastelao.org>
2026-09-15 13:05:59 +0200

    Added some of the exercises of the practice. WIP

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ManejadorFicheros manejador = new ManejadorFicheros();
+        System.out.println(manejador);
+    }
+}
---
+++ src/ManejadorFicheros.java
@@ -0,0 +1,77 @@
+import java.io.File;
+import java.io.IOException;
+
+public class ManejadorFicheros {
+
+    // 1) Comprueba si la ruta es un directorio existente
+    public String eDirectorio(String ruta){
+        File f = new File(ruta);
+        return (f.exists() && f.isDirectory()) ? "é directorio" : "non é directorio";
+    }
+
+    // 2) Comprueba si la ruta es un archivo existente
+    public String eFicheiro(String ruta){
+        File f = new File(ruta);
+        return (f.exists() && f.isFile()) ? "é ficheiro" : "non é ficheiro";
+    }
+
+    // 3) Crea un directorio solo si no existe previamente
+    public void creaDirectorio(String ruta){
+        File f = new File(ruta);
+        if (!f.exists()) {
+            boolean creado = f.mkdir();
+            if (creado) {
+                System.out.println("Directorio creado correctamente. ");
+            }
+        }
+    }
+
+    // 4) Crea un fichero en una ruta absoluta solo si el directorio existe y el archivo no
+    public void creaFicheiro(String dirName, String fileName) {
+        File dir = new File(dirName);
+        if (!dir.exists() && dir.isDirectory()) {
+            File file = new File(dir, fileName);
+            if (!file.exists()) {
+                try {
+                    if (file.createNewFile()) {
+                        System.out.println("Ficheiro creado correctamente");
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error ao crear o ficherio" + e.getMessage());
+                }
+            }
+        }
+    }
+
+    // 5) Muestra si el fichero tiene permisos de lectura y escritura
+    public void modoAcceso (String dirName, String fileName) {
+        File file = new File(dirName, fileName);
+        System.out.println(file.canWrite() ? "escritura si" : "escritura non");
+        System.out.println(file.canRead() ? "lectura si" : "lectura non");
+    }
+
+    // 6) Muestra la longitud del fichero en Bytes
+    public void calculaLonxitude(String dirName, String fileName) {
+        File file = new File(dirName, fileName);
+        System.out.println("Tamaño: " +file.length() + " bytes");
+    }
+
+    // 7) Cambia el archivo a solo lectura
+    public void mLectura (String dirName, String fileName) {
+        File file = new File(dirName, fileName);
+        if (file.exists()) {
+            file.setWritable(false);
+        }
+    }
+
+    // 8) Reestablece los permisos de escritura en el archivo
+    public void mEscritura (String dirName, String fileName) {
+        File file = new File(dirName, fileName);
+        if (file.exists()) {
+            file.setWritable(true);
+        }
+    }
+
+    // 9) Elimina el archivo o muestra mensaje de error si no existe
+
+}
